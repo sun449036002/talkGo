9933e67aae38fe59ece9c1acf90eda133a58edb5
mingming <449036002@qq.com>
2017-09-30 16:46:36 +0800

    记录数据库

--- models/msg.go
+++ models/msg.go
@@ -20,5 +20,6 @@ func init() {
 	// 注册驱动
 	orm.RegisterDataBase("default", "mysql", "root:mm@tcp(127.0.0.1:3306)/sun?charset=utf8");
 
-	orm.RunSyncdb("default", false, true);
+	dropOldTable := true;
+	orm.RunSyncdb("default", dropOldTable, true);
 }
