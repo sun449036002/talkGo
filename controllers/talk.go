8bfe72cf733598f44cd8c2f2d3120931e3837b0a
mingming <449036002@qq.com>
2017-10-19 20:32:32 +0800

    拍品按热度排序

--- controllers/talk.go
+++ controllers/talk.go
@@ -15,6 +15,8 @@ import (
 	. "talkGo/models"
 	"talkGo/lib"
 	"log"
+	"io/ioutil"
+	"os/exec"
 )
 
 
@@ -77,8 +79,34 @@ func (c *TalkController) UpVoice() {
 	}
 	fmt.Println(h);
 
+	//创建获取命令输出管道
+	cmd := exec.Command("/root/silk-v3-decoder/silk", "/root/go/src/talkGo/static/wx-file.silk", `/root/go/src/talkGo/static/wx-file.pcm`)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Printf("Error:can not obtain stdout pipe for command:%s\n", err)
+		return
+	}
+	//执行命令
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error:The command is err,", err)
+		return
+	}
+	//读取所有输出
+	bytes, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		fmt.Println("ReadAll Stdout:", err.Error())
+		return
+	}
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("wait:", err.Error())
+		return
+	}
+	fmt.Printf("stdout:\n\n %s", bytes)
+
+
 	//读取存储好的音频文件
-	voiceFile, err := os.Open("static/" + h.Filename);
+	voiceFile, err := os.Open("/root/go/src/talkGo/static/wx-file.pcm");
+	//voiceFile, err := os.Open("static/" + h.Filename);
 	if err != nil {
 		fmt.Println(err);
 	}
@@ -90,7 +118,7 @@ func (c *TalkController) UpVoice() {
 	if len > 0 {
 		fmt.Println(len);
 	}
-
+	//输出JSON
 	token := c.getToken();
 	jsonMap := make(map[string]string);
 	jsonMap["token"] = token;
