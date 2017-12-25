7130400380b3ff3ffa2621f2e793bb11541efd61
mingming <449036002@qq.com>
2017-12-25 17:35:24 +0800

    go

--- controllers/cartoon/index.go
+++ controllers/cartoon/index.go
@@ -27,10 +27,6 @@ func (c *IndexController) URLMapping() {
 // @router /list [get]
 func (c *IndexController) GetList() {
 	fmt.Println("this is get list")
-	c.Auth()
-}
-
-func (c *IndexController) Auth() {
 	/**
 	type=/category/weimanhua/kbmh 恐怖漫画
 	type=/category/weimanhua/gushimanhua 故事漫画
@@ -46,7 +42,8 @@ func (c *IndexController) Auth() {
 	type=/category/wanyi 玩艺
 	type=/category/chahua 插画
 	 */
-	res := httplib.Get("http://route.showapi.com/958-1?type=/category/weimanhua/kbmh&showapi_appid=" + beego.AppConfig.String("YiYuanAppId") + "&showapi_sign=" + beego.AppConfig.String("YiYuanSecretKey") + "&page=1")
+	page := c.GetString("page", "1")
+	res := httplib.Get("http://route.showapi.com/958-1?type=/category/weimanhua/kbmh&showapi_appid=" + beego.AppConfig.String("YiYuanAppId") + "&showapi_sign=" + beego.AppConfig.String("YiYuanSecretKey") + "&page=" + page)
 	//fmt.Println(res.String())
 
 	bts, err := res.Bytes()
@@ -61,6 +58,26 @@ func (c *IndexController) Auth() {
 	json := make(map[string]interface{})
 	json["list"] = contentList
 	json["hasMorePage"] = hasMorePage
+	c.Data["json"] = json
+
+	c.ServeJSON()
+}
+
+// getList 获取漫画内容...
+// @router /detail [get]
+func (c *IndexController) GetDetail() {
+	id := c.GetString("id")
+	res := httplib.Get("http://route.showapi.com/958-1?showapi_appid=" + beego.AppConfig.String("YiYuanAppId") + "&showapi_sign=" + beego.AppConfig.String("YiYuanSecretKey") + "&id=" + id)
+
+	bts, err := res.Bytes()
+	if(err != nil) {
+		fmt.Println(err)
+	}
+
+	//jsoniter.ParseString()
+	content := jsoniter.Get(bts, "showapi_res_body", "item").GetInterface()
+	json := make(map[string]interface{})
+	json["content"] = content
 	c.Data["json"] = json
 
 	c.ServeJSON()
