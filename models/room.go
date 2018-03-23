2dc29bb266948e4777a272bfeb737779547f1770
mingming <449036002@qq.com>
2018-03-23 15:23:25 +0800

    状态

--- models/room.go
+++ models/room.go
@@ -24,7 +24,7 @@ func (m *Room) GetList(page int64) ([]map[string]interface{}, int64, bool)  {
 	var pageSize int64 = 10
 	o := orm.NewOrm()
 	var roomList []Room
-	num, _ := o.QueryTable("room").Limit(pageSize, (page - 1) * pageSize).RelatedSel().OrderBy("-id").All(&roomList)
+	num, _ := o.QueryTable("room").Filter("status", 1).Limit(pageSize, (page - 1) * pageSize).RelatedSel().OrderBy("-id").All(&roomList)
 
 	list := make([]map[string]interface{}, len(roomList))
 	for k,room := range roomList {
@@ -43,6 +43,8 @@ func (m *Room) Create(userId int, name string) (int64, error) {
 	o := orm.NewOrm()
 	m.Name = name
 	m.UserId = userId
+	m.Status = 1
+
 	id, err := o.Insert(m)
 	if err != nil {
 		return 0, err
