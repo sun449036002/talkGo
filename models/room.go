bfcbe6a21375f09128c164dd2c0a0612b61124f7
mingming <449036002@qq.com>
2018-04-09 10:43:29 +0800

    mp3dir

--- models/room.go
+++ models/room.go
@@ -32,7 +32,7 @@ func (m *Room) GetList(page int64) ([]map[string]interface{}, int64, bool)  {
 		tplMap := make(map[string]interface{})
 		tplMap["roomId"] = "room_"  + strconv.Itoa(room.Id)
 		tplMap["roomName"] = room.Name
-		tplMap["roomCover"] = beego.AppConfig.String("rooturl") + "static/hlsCover/" + room.Name + "_cover.png"
+		tplMap["roomCover"] = beego.AppConfig.String("rooturl") + "mp3dir/hlsCover/" + room.Name + "_cover.png"
 		list[k] = tplMap
 	}
 
