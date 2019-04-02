fdc9bba39d0fb3836d37958bded124664c5ced15
Administrator <449036002@qq.com>
2019-04-02 17:11:27 +0800

    'timer'

--- controllers/riddle.go
+++ controllers/riddle.go
@@ -1,6 +1,12 @@
 package controllers
 
-import "time"
+import (
+	"time"
+	"fmt"
+	"github.com/astaxie/beego"
+	"github.com/json-iterator/go"
+	"github.com/astaxie/beego/httplib"
+)
 
 /**
 谜语
@@ -18,13 +24,38 @@ func (c *RiddleController) URLMapping() {
 // @router /get [get]
 func (c *RiddleController) Get() {
 	jsonMap := make(map[string]interface{})
+	//roomId := c.GetString("roomId")
+	myType := c.GetString("type")
 
 	now := time.Now()
 	m := now.Minute()
-	liveTimers := 600 - (m % 10)*60 - now.Second()
+	liveTimers := 600 - (m % 10) * 60 - now.Second()
+
+	if liveTimers > 0 && myType == "timer" {
+		jsonMap["code"] = 0
+		jsonMap["liveTimers"] = liveTimers
+
+		c.Data["json"] = jsonMap
+		c.ServeJSON()
+		return
+	}
+
+
+	//API 获取谜语
+	riddleUrl := "http://route.showapi.com/151-2?" + "showapi_appid=" + beego.AppConfig.String("YiYuanAppId") + "&showapi_sign=" + beego.AppConfig.String("YiYuanSecretKey")
+	fmt.Println("riddleUrl is : ", riddleUrl)
+	res := httplib.Get(riddleUrl)
+
+	bts, err := res.Bytes()
+	if err != nil {
+		fmt.Println("err =====> ", err)
+	}
+
+
+	firstRiddle := jsoniter.Get(bts, "showapi_res_body", "pagebean", "contentlist").Get(0).GetInterface()
 
 	jsonMap["code"] = 0
-	jsonMap["liveTimers"] = liveTimers
+	jsonMap["riddle"] = firstRiddle
 
 	c.Data["json"] = jsonMap
 	c.ServeJSON()
